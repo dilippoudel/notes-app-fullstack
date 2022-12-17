5205dfde2d5d026919b2a8c04b01b93190614a35
Dilip <dilippoudel578@gmail.com>
2022-12-17 19:31:05 +0200

    updated

--- index.js
+++ index.js
@@ -18,20 +18,26 @@ app.get('/api/notes', (req, res) => {
   })
 })
 
-app.get('/api/notes/:id', async (req, res) => {
-  const note = await Note.findById(req.params.id)
-  if (note) {
-    res.send(note)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/notes/:id', async (req, res, next) => {
+  Note.findById(req.params.id)
+    .then((note) => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch((err) => next(err))
 })
 
 // deleting the note resource
-app.delete('/api/notes/:id', async (req, res) => {
-  const result = await Note.findByIdAndDelete(req.params.id)
-
-  res.status(204).end()
+app.delete('/api/notes/:id', async (req, res, next) => {
+  try {
+    await Note.findByIdAndRemove(req.params.id)
+    res.status(204).end()
+  } catch (error) {
+    next(error)
+  }
 })
 
 // creating new note
@@ -50,20 +56,36 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-// updating note
-app.put('/api/notes/:id', async (request, response) => {
-  const note = Note.findByIdAndUpdate(
-    request.params.id,
-    { $set: request.body },
-    (err) => {
-      if (err) {
-        return response.json({ error: 'Something went wrong' })
-      }
-      response.send('product updated')
-    },
-  )
-  console.log('request body is ', request.body)
+// updating note // toogling importance of note
+app.put('/api/notes/:id', async (request, response, next) => {
+  try {
+    const body = request.body
+    console.log('body is ', body)
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+    const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, {
+      new: true,
+    })
+    response.json({ updatedNote })
+  } catch (error) {
+    next(error)
+  }
 })
+
+// example of middle ware for error handling
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT)
