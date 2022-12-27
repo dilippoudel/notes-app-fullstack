c7b0f84ebc1e409e226c79271b09d054067853d6
Dilip <dilippoudel578@gmail.com>
2022-12-27 10:36:44 +0200

    Add test files and refactor codes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 logger.info('connecting to', config.MONGODB_URI)
+mongoose.set('strictQuery', true)
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -16,6 +17,7 @@ mongoose
   .catch((error) => {
     logger.error('error connecting to MongoDB:', error.message)
   })
+
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
--- controllers/notes.js
+++ controllers/notes.js
@@ -1,22 +1,26 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (req, res) => {
-  Note.find({}).then((notes) => {
+notesRouter.get('/', async (req, res, next) => {
+  try {
+    const notes = await Note.find({})
     res.json(notes)
-  })
+  } catch (error) {
+    next(error)
+  }
 })
 
 notesRouter.get('/:id', async (req, res, next) => {
-  Note.findById(req.params.id)
-    .then((note) => {
-      if (note) {
-        res.json(note)
-      } else {
-        res.status(404).end()
-      }
-    })
-    .catch((err) => next(err))
+  const note = await Note.findById(req.params.id)
+  try {
+    if (note) {
+      res.json(note)
+    } else {
+      res.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 // deleting the note resource
@@ -30,30 +34,30 @@ notesRouter.delete('/:id', async (req, res, next) => {
 })
 
 // creating new note
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
   const body = request.body
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
   })
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote)
-    })
-    .catch((error) => next(error))
+  try {
+    const savedNote = await note.save()
+    response.status(201).json(savedNote)
+  } catch (error) {
+    next(error)
+  }
 })
 
 // updating note // toogling importance of note
 notesRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+  console.log('body is ', body)
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
   try {
-    const body = request.body
-    console.log('body is ', body)
-    const note = {
-      content: body.content,
-      important: body.important,
-    }
     const updatedNote = await Note.findByIdAndUpdate(request.params.id, note, {
       new: true,
       runValidators: true,
