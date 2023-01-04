d21aec2a715d5568983039092b86382945532620
Dilip <dilippoudel578@gmail.com>
2023-01-04 20:32:40 +0200

    updated

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
-const jwt = require('jsonwebtoken')
 const notesRouter = require('express').Router()
 const User = require('../models/user')
 const Note = require('../models/note')
+const middleware = require('../utils/middleware')
 
 notesRouter.get('/', async (req, res) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
@@ -23,34 +23,31 @@ notesRouter.delete('/:id', async (req, res) => {
   await Note.findByIdAndRemove(req.params.id)
   res.status(204).end()
 })
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer')) {
-    return authorization.substring(7)
-  }
-  return null
-}
+
 // creating new note
-notesRouter.post('/', async (request, response) => {
-  const body = request.body
-  const token = getTokenFrom(request)
-  // eslint-disable-next-line no-undef
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
-  }
-  const user = await User.findById(body.userId)
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    user: user._id,
-  })
-  const savedNote = await note.save()
-  user.notes = user.notes.concat(savedNote._id)
-  await user.save()
-  response.status(201).json(savedNote)
-})
+notesRouter.post(
+  '/',
+  middleware.tokenExtractor,
+  middleware.userExtractor,
+  async (request, response) => {
+    const body = request.body
+    // eslint-disable-next-line no-undef
+    if (!request.token) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(request.user)
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      user: user._id,
+    })
+    const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+    response.status(201).json(savedNote)
+  },
+)
 
 // updating note // toogling importance of note
 notesRouter.put('/:id', async (request, response) => {
